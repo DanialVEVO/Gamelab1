c11b28eddb0e2f7ee042ab473176babc69682046
Chan92 <angell0vee@hotmail.com>
2015-10-28 16:23:38 +0100

    Bug log update fixes, unity add script

---
+++ Unity/Assets/Programming/Chantal/Scripts in Process/MoveLevelScript.cs
@@ -0,0 +1,18 @@
+﻿/* 7S_NXTL_001
+ * Changing Level Scene Script
+ * Scripted by Chantal
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class MoveLevelScript : MonoBehaviour {
+
+	public int levelId;
+
+	void OnCollisionEnter (Collision coll) {
+		if (coll.gameObject.tag == "Player"){
+			Application.LoadLevel(levelId);
+		}
+	}
+}
