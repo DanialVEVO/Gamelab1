a9e900f2d61976dcf018c098a541ae12b10f65eb
Chan92 <angell0vee@hotmail.com>
2015-12-08 09:37:39 +0100

    StartScene & Canvas update + Uren reg

--- Unity/Assets/Programming/Scripts Done/UI/GuiScript.cs
+++ Unity/Assets/Programming/Scripts Done/UI/GuiScript.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using System.Collections;
 
 public class GuiScript : MonoBehaviour {
-
+	
 	public enum MenuPanels {
 		MainMenu,       // 0
 		GameMenu,       // 1
@@ -18,67 +18,73 @@ public class GuiScript : MonoBehaviour {
 		HelpScreen,     // 6
 		GamePlay        // 7
 	}
-
+	
 	public MenuPanels menuPanels;
 	public GameObject[] menuObjArray;
 	public bool inPause;
 	public SaveLoadGameScript saveLoadGameScr;
-
+	
 	void Update() {
 		PanelSwitchActive();
 		SwitchPauseMode();
 	}
-
+	
 	public void PanelSwitch(int newMenuPanel) {
 		menuPanels = (MenuPanels)newMenuPanel;
 		PanelSwitchActive();
 	}
-
+	
 	void PanelSwitchActive() {
 		MenuOff();
 		switch (menuPanels) {
-			case MenuPanels.MainMenu:
-				menuObjArray[0].SetActive(true);
-				break;
-			case MenuPanels.GameMenu:
-				menuObjArray[1].SetActive(true);
-				break;
-			case MenuPanels.Pause:
-				menuObjArray[2].SetActive(true);
-				break;
-			case MenuPanels.Options:
-				menuObjArray[3].SetActive(true);
-				break;
-			case MenuPanels.GameOptions:
-				menuObjArray[4].SetActive(true);
-				break;
-			case MenuPanels.Credits:
-				menuObjArray[5].SetActive(true);
-				break;
-			case MenuPanels.HelpScreen:
-				menuObjArray[6].SetActive(true);
-				break;
+		case MenuPanels.MainMenu:
+			menuObjArray[0].SetActive(true);
+			break;
+		case MenuPanels.GameMenu:
+			menuObjArray[1].SetActive(true);
+			break;
+		case MenuPanels.Pause:
+			menuObjArray[2].SetActive(true);
+			break;
+		case MenuPanels.Options:
+			menuObjArray[3].SetActive(true);
+			break;
+		case MenuPanels.GameOptions:
+			menuObjArray[4].SetActive(true);
+			break;
+		case MenuPanels.Credits:
+			menuObjArray[5].SetActive(true);
+			break;
+		case MenuPanels.HelpScreen:
+			menuObjArray[6].SetActive(true);
+			break;
 		}
 	}
-
+	
 	void MenuOff() {
 		for (int i = 0; i < menuObjArray.Length; i++) {
 			menuObjArray[i].SetActive(false);
 		}
 	}
-
+	
 	public void ExitGame() {
 		Application.Quit();
 	}
-
+	
 	public void StartNewGame() {
 		Application.LoadLevel(1);
 	}
-
+	
 	public void StartSavedGame() {
 		saveLoadGameScr.LoadGame();
 	}
-
+	
+	public void SaveCurrentGame(){
+		if (Application.loadedLevel != 0){
+			saveLoadGameScr.SaveGame();
+		}
+	}
+	
 	public void SwitchPauseMode() {
 		if (Input.GetButtonDown("Pause")) {
 			if (inPause == false && menuPanels == MenuPanels.GamePlay) {
@@ -92,5 +98,4 @@ public class GuiScript : MonoBehaviour {
 			}
 		}
 	}
-
 }
