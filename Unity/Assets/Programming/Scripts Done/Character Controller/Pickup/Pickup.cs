c74c29a39443304103b7f94d9001eb2dafac0f4f
Danial <danial.azimi.a@gmail.com>
2016-01-12 09:15:15 +0100

    Color Randomizer & Fonts

--- Unity/Assets/Programming/Scripts Done/Character Controller/Pickup/Pickup.cs
+++ Unity/Assets/Programming/Scripts Done/Character Controller/Pickup/Pickup.cs
@@ -4,12 +4,16 @@ using UnityEngine.UI;
 
 public class Pickup : MonoBehaviour {
 
-	public int pickupCount;
-	public int maxPickupCount;
-	public Text pickupText;
+	public int 			pickupCount;
+	public int 			maxPickupCount;
+	public Text 		pickupText;
+
+	public float 		uiResetTimer;
+	public GameObject	uiCollect;
 
 	void OnTriggerEnter (Collider collision){
 		if(collision.GetComponent<Collider>().tag == "Pickup"){
+			uiCollect.GetComponent<UiCollectScript>().countdownTimer = uiResetTimer;
 			pickupCount ++;
 			//print("pickupCount");
 			collision.GetComponent<FloatingObjectsScript>().DestroyThis();
