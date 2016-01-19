5afe1d0add6788e4a67f770a7b631daa6f05d369
Danial <danial.azimi.a@gmail.com>
2016-01-19 12:08:15 +0100

    Level 1 edit, Sarah materials

--- Unity/Assets/Programming/Scripts Done/Enemy/EnemyWaveSpawner.cs
+++ Unity/Assets/Programming/Scripts Done/Enemy/EnemyWaveSpawner.cs
@@ -1,38 +1,38 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class EnemyWaveSpawner : MonoBehaviour {
-
-public int waveNumber;
-
-
-	private int enemySpawnCountPerWave = 5;
-	private int spawnCount;
-
-
-	public GameObject enemyPrefab;
-
-
-	public Transform[] spawnLocations;
-
-
-	public List<GameObject> _list = new List<GameObject>();
-
-
-	public void SpawnWave (){
-		GameObject stufToSpawn;
-		spawnCount = CalculateNumberOfEnemys(enemySpawnCountPerWave);
-		for(int i = 0 ; i <= spawnCount; i++){		
-			stufToSpawn = Instantiate(enemyPrefab, spawnLocations[Random.Range(0, spawnLocations.Length).position , Quaternion.identity) as GameObject;
-			_list.Add(stufToSpawn);
-		}
-		waveNumber ++;
-	}
-
-	int CalculateNumberOfEnemys (int _enemySpawnCount){
-		int spawnCount = (_enemySpawnCount * waveNumber);
-
-		return spawnCount;
-	}
-
-}
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class EnemyWaveSpawner : MonoBehaviour {
+//
+//public int waveNumber;
+//
+//
+//	private int enemySpawnCountPerWave = 5;
+//	private int spawnCount;
+//
+//
+//	public GameObject enemyPrefab;
+//
+//
+//	public Transform[] spawnLocations;
+//
+//
+//	public List<GameObject> _list = new List<GameObject>();
+//
+//
+//	public void SpawnWave (){
+//		GameObject stufToSpawn;
+//		spawnCount = CalculateNumberOfEnemys(enemySpawnCountPerWave);
+//		for(int i = 0 ; i <= spawnCount; i++){		
+//			stufToSpawn = Instantiate(enemyPrefab, spawnLocations[Random.Range(0, spawnLocations.Length).position , Quaternion.identity) as GameObject;
+//			_list.Add(stufToSpawn);
+//		}
+//		waveNumber ++;
+//	}
+//
+//	int CalculateNumberOfEnemys (int _enemySpawnCount){
+//		int spawnCount = (_enemySpawnCount * waveNumber);
+//
+//		return spawnCount;
+//	}
+//
+//}
