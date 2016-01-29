c9e5b5bf912ea4aa50f2e5a34790ad8af9412972
Chan92 <angell0vee@hotmail.com>
2016-01-29 11:52:39 +0100

    scr

--- Unity/Assets/Programming/Scripts Done/GameManager/MoveLevelScript.cs
+++ Unity/Assets/Programming/Scripts Done/GameManager/MoveLevelScript.cs
@@ -17,24 +17,24 @@ public class MoveLevelScript : MonoBehaviour {
 	private int curSplash = 0;
 
 	void Start (){
-		splashScreen = GameObject.Find("SplashImg");
-		splashScreen.SetActive(false);	
+	//	splashScreen = GameObject.Find("SplashImg");
+		//splashScreen.SetActive(false);	
 	}
 
 	void OnCollisionEnter (Collision coll) {
 	//	if (coll.gameObject.transform.tag == "Player"){
 			GameObject enemiesListInScene = GameObject.FindWithTag("Enemies List");
 			Destroy(enemiesListInScene);
-			if (splashTimer <= 0){
-				splashScreen.SetActive(true);
-				if(curSplash < splashImg.Length){
-					splashScreen.GetComponent<Image>().sprite = splashImg[curSplash];
-					splashTimer = splashTimerRes;
-					curSplash++;
-				} else {
+	//		if (splashTimer <= 0){
+		//		splashScreen.SetActive(true);
+	//			if(curSplash < splashImg.Length){
+	//				splashScreen.GetComponent<Image>().sprite = splashImg[curSplash];
+	//				splashTimer = splashTimerRes;
+	//				curSplash++;
+	//			} else {
 					Application.LoadLevel(nextLevelId);
 				}
-			}
+	//		}
 	//	}
 	}
 }
