d48ab11ccbacb18c83994620628ae937059d79c6
Chan92 <angell0vee@hotmail.com>
2016-01-29 08:48:44 +0100

    Melee dmg scr edit

--- Unity/Assets/Programming/Scripts Done/Abilities/MeleeDmgScript.cs
+++ Unity/Assets/Programming/Scripts Done/Abilities/MeleeDmgScript.cs
@@ -7,26 +7,48 @@ using UnityEngine;
 using System.Collections;
 
 public class MeleeDmgScript : KnockBackScript {
-	
+
+	public RandomParticleSelector randomParticle;
+	public GameObject cowboyAbility;
+	public GameObject heroAbility;
+	public GameObject astroAbility;
 	public int dmg = 1;
 	public Animator anim;
 	public AudioClip attackSound;
-	public float swordLength = 1f;
+	public AudioClip saberSound;
+	public AudioClip[] punchSound;
+	public float hitRange = 2f;
 	public RaycastHit hit;
+	public Vector3 rayOffset = new Vector3(0, 1f, 0);
 
 	void Update () {
+
+		//code not very clean due last minute fix
 		if(Input.GetButtonDown("Fire1")){
-			anim.SetBool("Attack1", true);
-			GetComponent<AudioSource>().PlayOneShot(attackSound);
-			CheckDmg();
+			if (cowboyAbility.activeInHierarchy == false){
+				anim.SetBool("Attack1", true);
+				if (heroAbility.activeInHierarchy == false){
+					if (astroAbility.activeInHierarchy == false){
+						GetComponent<AudioSource>().PlayOneShot(attackSound);
+						CheckDmg();
+					} else {
+						GetComponent<AudioSource>().PlayOneShot(saberSound);
+						CheckDmg();
+					}
+				} else {
+					randomParticle.ParticleRandomizer();
+					GetComponent<AudioSource>().PlayOneShot(punchSound[randomParticle.selectedInt]);
+					CheckDmg();
+				}
+			}
 		} else if (Input.GetButtonUp("Fire1")){
 			anim.SetBool("Attack1", false);
 		}
 	}
 
 	public void CheckDmg(){
-		Debug.DrawRay(transform.position, transform.forward, Color.green, swordLength);
-		if(Physics.Raycast(transform.position, transform.forward, out hit, swordLength)){
+		Debug.DrawRay(transform.position + rayOffset, transform.forward, Color.green, hitRange);
+		if(Physics.Raycast(transform.position + rayOffset, transform.forward, out hit, hitRange)){
 			if (hit.transform.tag == "Enemy"){
 				GiveDmg(hit.transform.gameObject);
 				KnockBack(hit.transform.GetComponent<Collider>());
